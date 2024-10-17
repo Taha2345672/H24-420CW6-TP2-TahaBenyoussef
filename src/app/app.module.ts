c92933e24d6bccfaec785039b9a37fd8a687bf7f
taha <2345672@cegepmontpetit.ca>
2024-10-17 19:36:55 -0400

    FCT-update css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppComponent } from './app.component';
 import { AlbumComponent } from './album/album.component';
 import { ArtistComponent } from './artist/artist.component';
 import { ShowComponent } from './show/show.component';
-import { SongComponent } from '.c:/Users/2345672/Desktop/H24-420CW6-TP2-TahaBenyoussef/H24-420CW6-TP2-TahaBenyoussef/src/song/song.component';
 import { SongComponent } from './song/song.component';
 
 @NgModule({

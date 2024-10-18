5cea2d4ac098d5490ab187d799f8c331207b69be
taha <2345672@cegepmontpetit.ca>
2024-10-17 20:15:30 -0400

    FCT_correction css

--- src/app/album/album.component.ts
+++ src/app/album/album.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-album',
   templateUrl: './album.component.html',
   styleUrls: ['./album.component.css']
+  
 })
 export class AlbumComponent implements OnInit {
 

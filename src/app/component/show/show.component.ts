f84790515157c6c516c618c5311dcc608f47a376
toto <2345672@cegepmontpetit.ca>
2024-10-26 11:28:20 -0400

    Deub_debogage

--- src/app/component/show/show.component.ts
+++ src/app/component/show/show.component.ts
@@ -4,7 +4,7 @@ import { ActivatedRoute } from '@angular/router';
 import { GoogleMap } from '@angular/google-maps';
 import { SpotifyService } from '../../services/Spotify.service';
 
-declare var google: any; 
+declare var google: any;
 
 @Component({
   selector: 'app-show',
@@ -13,42 +13,56 @@ declare var google: any;
 })
 export class ShowComponent implements OnInit {
   artistName: string | null = null;
-  inputLat : number = 0;
-  inputLng : number = 0;
+  inputLat: number = 0;
+  inputLng: number = 0;
 
-  center : google.maps.LatLngLiteral = {lat: 42, lng: -4};
-  zoom : number = 5;
+  center: google.maps.LatLngLiteral = { lat: 42, lng: -4 };
+  zoom: number = 5;
 
-  markerPositions :google.maps.LatLngLiteral[]= [{lat: 42, lng: -4}]
+  markerPositions: google.maps.LatLngLiteral[] = [{ lat: 42, lng: -4 }];
 
-  constructor(public http: HttpClient, public spotify: SpotifyService, public route : ActivatedRoute ) { }  
+  constructor(public http: HttpClient, public spotify: SpotifyService, public route: ActivatedRoute) {}
 
-  async ngOnInit() {   
-
-    this.artistName = await this.route.snapshot.paramMap.get("name")
+  async ngOnInit() {
+    this.artistName = await this.route.snapshot.paramMap.get("name");
 
     if (this.artistName) {
-      this.spotify.getConcert(this.artistName).then(() => {this.initializeMap();
-      });         
-    }  
+      await this.spotify.getConcert(this.artistName);
+      this.initializeMap();
+    }
   }
+
   initializeMap() {
     const mapOptions = {
       center: { lat: 42, lng: -4 },
       zoom: 5
     };
     const mapElement = document.getElementById('map');
+    
+    
     if (mapElement) {
+      console.log(mapOptions);
       const map = new google.maps.Map(mapElement, mapOptions);
       
-      this.spotify.concerts.forEach((show: any) => {
-        const marker = new google.maps.Marker({
-          position: { lat: show.venue.latitude, lng: show.venue.longitude },
-          map: map,
-          title: show.venue.name
-        });
-      });
+      
+      if (this.spotify.concerts && this.spotify.concerts.length > 0) {
+        console.log(this.spotify.concerts);
+        
+      
+        for (const show of this.spotify.concerts) {
+          console.log(show);
+          const marker = new google.maps.Marker({
+            position: { lat: show.venue.latitude, lng: show.venue.longitude },
+            map: map,
+            title: show.venue.name
+          });
+        }
+      }
     }
   }
 }
+        
+      
+    
+  
 
--- src/app/services/Spotify.service.ts
+++ src/app/services/Spotify.service.ts
@@ -5,7 +5,6 @@ import { Album } from '../models/album';
 import { Song } from '../models/song';
 import { Artist } from '../models/artist';
 
-
 const CLIENT_ID = "d5b0eafe720f49f1b7d026fafc1f9c5e";
 const CLIENT_SECRET = "61fa174064ff4e439c9fbc6759a3f105";
 const GOOGLE_API_KEY = "AIzaSyA60J7T5sDqgcpviaBchjvyCfQxjolDL9A";
@@ -16,142 +15,118 @@ const YOUTUBE_LINK = "https://youtube.com/embed/";
 })
 export class SpotifyService {
   spotifyToken?: string;
-
-  jsonData : string | null = null;
-  listArtiste: Artist [] = [];
-  artistName : string = "";
-
-  listAlbum: Album [] = [];
-  listSong : Song[] = [];
-  albumName : string = "";
-
+  artistName: string = ""; 
+  listArtiste: Artist[] = [];
+  listAlbum: Album[] = [];
+  listSong: Song[] = [];
   concerts: any[] = [];
-  
 
-constructor(public http: HttpClient) {}
+  constructor(public http: HttpClient) { }
 
+  async connect(): Promise<void> {
+    let body = new HttpParams()
+      .set('grant_type', 'client_credentials');
 
-async connect(): Promise<void> {
-let body = new HttpParams()
-.set('grant_type', 'client_credentials');
-let httpOptions = {
-headers: new HttpHeaders({
-  'Content-Type': 'application/x-www-form-urlencoded',
-  'Authorization': 'Basic ' + btoa(CLIENT_ID + ':' + CLIENT_SECRET)})
-};
+    let httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Basic ' + btoa(CLIENT_ID + ':' + CLIENT_SECRET)
+      })
+    };
 
-let response = await lastValueFrom(this.http.post<any>('https://accounts.spotify.com/api/token',      
-body.toString(), httpOptions));
-console.log(response);
-this.spotifyToken = response.access_token;
- localStorage.setItem("Key", JSON.stringify(this.spotifyToken));
-}
-
-async searchArtist(artist : string): Promise<any> {
-  const httpOptions = {headers: new HttpHeaders({
-  'Content-Type': 'application/json',
-  'Authorization': 'Bearer ' + this.spotifyToken})
-  };
-  let response = await lastValueFrom(this.http.get<any>('https://api.spotify.com/v1/search?type=artist&offset=0&limit=1&q=' + artist, httpOptions));
-  console.log(response);
-  
-  this.listArtiste.push(new Artist(response.artists.items[0].id, response.artists.items[0].name,response.artists.items[0].images[0].url));
-
-  localStorage.setItem("Artiste", JSON.stringify(this.listArtiste));
-  return this.listArtiste;
-
+    let response = await lastValueFrom(this.http.post<any>('https://accounts.spotify.com/api/token', body.toString(), httpOptions));
+    this.spotifyToken = response.access_token;
+    localStorage.setItem("Key", JSON.stringify(this.spotifyToken));
   }
-  async addArtist(){
-    this.listArtiste = await this.searchArtist(this.artistName)
-    console.log(this.listArtiste);
-   }
+
+  async searchArtist(artist: string): Promise<any> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + this.spotifyToken
+      })
+    };
+    
+    let response = await lastValueFrom(this.http.get<any>('https://api.spotify.com/v1/search?type=artist&offset=0&limit=1&q=' + artist, httpOptions));
+    
+    if (response.artists.items.length > 0) {
+      const artistData = response.artists.items[0];
+      this.listArtiste.push(new Artist(artistData.id, artistData.name, artistData.images[0].url));
+      localStorage.setItem("Artiste", JSON.stringify(this.listArtiste));
+    }
+    
+    return this.listArtiste;
+  }
+
+  async addArtist() {
+    this.listArtiste = await this.searchArtist(this.artistName);
+  }
   
-   clearFavoris(){
-     this.listArtiste = [];
-     localStorage.setItem("Artiste", JSON.stringify(this.listArtiste));
-   } 
+  clearFavoris() {
+    this.listArtiste = [];
+    localStorage.setItem("Artiste", JSON.stringify(this.listArtiste));
+  } 
 
   async getAlbums(artistId: string): Promise<any> {
-  const httpOptions = {
-  headers: new HttpHeaders({
-    'Content-Type': 'application/json',
-    'Authorization': 'Bearer ' + this.spotifyToken})
-  };
-  
-  let response = await lastValueFrom(this.http.get<any>("https://api.spotify.com/v1/artists/" + artistId + "/albums?include_groups=album,single", httpOptions));
-  console.log(response);
-   
-  for(let i = 0; i < response.items.length; i++){
-
-    this.listAlbum.push(new Album(response.items[i].id, response.items[i].name, response.items[i].images[1].url));  
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + this.spotifyToken
+      })
+    };
+    
+    let response = await lastValueFrom(this.http.get<any>("https://api.spotify.com/v1/artists/" + artistId + "/albums?include_groups=album,single", httpOptions));
+    
+    for (let i = 0; i < response.items.length; i++) {
+      this.listAlbum.push(new Album(response.items[i].id, response.items[i].name, response.items[i].images[1].url));  
+    }
+    
+    localStorage.setItem("Album", JSON.stringify(this.listAlbum));
+    return this.listAlbum;
   }
-  localStorage.setItem("Album", JSON.stringify(this.listAlbum));
-  return this.listAlbum;
-   }
    
-async addAlbum(artistId: string){
-  
-  this.listAlbum = [];
-  this.listArtiste = [];
-  
-  localStorage.setItem("Album", JSON.stringify(this.listAlbum));
-   
-  this.listAlbum = await this.getAlbums(artistId);
-  console.log(this.listAlbum);
-}
-   
-   async getSongs(albumId: string): Promise<Song[]> {
-   const httpOptions = {
-   headers: new HttpHeaders({
-    'Content-Type': 'application/json',
-    'Authorization': 'Bearer ' + this.spotifyToken
-    })
-   };
-    let x = await lastValueFrom(this.http.get<any>("https://api.spotify.com/v1/albums/" + albumId, httpOptions));
-   console.log(x);
-
-    for(let i = 0; i < x.tracks.items.length; i++){
-    this.listSong.push(new Song (x.tracks.items[i].id, x.tracks.items[i].name));
+  async addAlbum(artistId: string) {
+    this.listAlbum = [];
+    this.listArtiste = [];
+    localStorage.setItem("Album", JSON.stringify(this.listAlbum));
+    this.listAlbum = await this.getAlbums(artistId);
   }
-  ​
-  return this.listSong;
+   
+  async getSongs(albumId: string): Promise<Song[]> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + this.spotifyToken
+      })
+    };
+    
+    let response = await lastValueFrom(this.http.get<any>("https://api.spotify.com/v1/albums/" + albumId, httpOptions));
+    
+    for (let i = 0; i < response.tracks.items.length; i++) {
+      this.listSong.push(new Song(response.tracks.items[i].id, response.tracks.items[i].name));
+    }
+    
+    return this.listSong;
   }
     
-async addSong(albumId: string){
-  this.listAlbum = [];
-  this.listSong = [];
-  
-    console.log(albumId);
-    console.log(this.listSong);
-
+  async addSong(albumId: string) {
+    this.listAlbum = [];
+    this.listSong = [];
     this.listSong = await this.getSongs(albumId);
-  
-}
+  }
 
-async searchVideoId(songName : string) : Promise<string>{
-  
-  
-  let RESPONSE = await lastValueFrom(this.http.get<any>("https://www.googleapis.com/youtube/v3/search?type=video&part=id&maxResults=1&key="+ GOOGLE_API_KEY + "&q=" + songName));
-  console.log(RESPONSE);  
-  let videoId = RESPONSE.items[0].id.videoId;  
-  
-console.log(videoId);
-  return videoId;  
-}
+  async searchVideoId(songName: string): Promise<string> {
+    let response = await lastValueFrom(this.http.get<any>("https://www.googleapis.com/youtube/v3/search?type=video&part=id&maxResults=1&key=" + GOOGLE_API_KEY + "&q=" + songName));
+    return response.items.length > 0 ? response.items[0].id.videoId : '';
+  }
 
-async getConcert(artistName: string){
-  this.listAlbum = [];
-  this.listArtiste = [];
-  this.concerts = [];
+  async getConcert(artistName: string) {
+    this.listAlbum = [];
+    this.listArtiste = [];
+    this.concerts = [];
 
-  let concert = await lastValueFrom(this.http.get<any>(`https://rest.bandsintown.com/artists/${artistName}/events?app_id=YOUR_APP_ID`))
-
-  console.log(concert);
-  this.concerts = concert; 
-  
- 
-  return concert; 
-  
-}
-
+    let concert = await lastValueFrom(this.http.get<any>(`https://rest.bandsintown.com/artists/${artistName}/events?app_id=YOUR_APP_ID`));
+    this.concerts = concert; 
+    return concert; 
+  }
 }

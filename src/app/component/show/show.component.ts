097f51b769de09accd583784d7dcc3c483357f59
toto <2345672@cegepmontpetit.ca>
2024-10-28 11:16:08 -0400

    FCT_debogage

--- src/app/component/show/show.component.ts
+++ src/app/component/show/show.component.ts
@@ -22,7 +22,6 @@ export class ShowComponent implements OnInit {
 
   markerPositions: google.maps.LatLngLiteral[] = [{ lat: 42, lng: -4 }];
 
-<<<<<<< HEAD
   constructor(public http: HttpClient, public spotify: SpotifyService, public route: ActivatedRoute, public translate: TranslateService) {}
 
   async ngOnInit() {
@@ -32,16 +31,6 @@ export class ShowComponent implements OnInit {
     if (this.artistName) {
       await this.spotify.getConcert(this.artistName);
       console.log( this.spotify.concerts); 
-=======
-  constructor(public http: HttpClient,public spotify: SpotifyService,public route: ActivatedRoute,public translate: TranslateService 
-  ) {}
-
-  async ngOnInit() {
-    this.artistName = await this.route.snapshot.paramMap.get("name");
-
-    if (this.artistName) {
-      await this.spotify.getConcert(this.artistName);
->>>>>>> 3d481de9b3fbc694c79aeb7ad56d9542fe467e0a
       this.initializeMap();
     }
   }
@@ -55,33 +44,21 @@ export class ShowComponent implements OnInit {
 
     if (mapElement) {
       const map = new google.maps.Map(mapElement, mapOptions);
-<<<<<<< HEAD
       console.log( mapOptions); 
 
       if (this.spotify.concerts && this.spotify.concerts.length > 0) {
         for (const show of this.spotify.concerts) {
           console.log( show.venue.name, show.venue.latitude, show.venue.longitude); 
-=======
-
-      if (this.spotify.concerts && this.spotify.concerts.length > 0) {
-        for (const show of this.spotify.concerts) {
->>>>>>> 3d481de9b3fbc694c79aeb7ad56d9542fe467e0a
           const marker = new google.maps.Marker({
             position: { lat: show.venue.latitude, lng: show.venue.longitude },
             map: map,
             title: show.venue.name
           });
         }
-<<<<<<< HEAD
    
 
     }
   }
   }
 
-=======
-      }
-    }
-  }
->>>>>>> 3d481de9b3fbc694c79aeb7ad56d9542fe467e0a
 }

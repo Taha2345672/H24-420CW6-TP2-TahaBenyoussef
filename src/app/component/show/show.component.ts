31aba1037d52b9ff73af0201ea7e6f140573eee9
toto <2345672@cegepmontpetit.ca>
2024-10-28 12:11:10 -0400

    FCT_Debogage

--- src/app/component/show/show.component.ts
+++ src/app/component/show/show.component.ts
@@ -1,9 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { GoogleMap } from '@angular/google-maps';
 import { SpotifyService } from '../../services/Spotify.service';
-import { TranslateService } from '@ngx-translate/core'; 
+import { TranslateService } from '@ngx-translate/core';
 
 declare var google: any;
 
@@ -14,51 +13,48 @@ declare var google: any;
 })
 export class ShowComponent implements OnInit {
   artistName: string | null = null;
-  inputLat: number = 0;
-  inputLng: number = 0;
-
   center: google.maps.LatLngLiteral = { lat: 42, lng: -4 };
   zoom: number = 5;
+  markerPositions: google.maps.LatLngLiteral[] = [];
 
-  markerPositions: google.maps.LatLngLiteral[] = [{ lat: 42, lng: -4 }];
-
-  constructor(public http: HttpClient, public spotify: SpotifyService, public route: ActivatedRoute, public translate: TranslateService) {}
+  constructor(
+    public http: HttpClient,
+    public spotify: SpotifyService,
+    public route: ActivatedRoute,
+    public translate: TranslateService
+  ) {}
 
   async ngOnInit() {
-    this.artistName = await this.route.snapshot.paramMap.get("artistName");
-    console.log( this.artistName); 
+    this.artistName = this.route.snapshot.paramMap.get("artistName");
+    console.log(this.artistName);
 
     if (this.artistName) {
       await this.spotify.getConcert(this.artistName);
-      console.log( this.spotify.concerts); 
+      console.log(this.spotify.concerts);
       this.initializeMap();
     }
   }
 
   initializeMap() {
     const mapOptions = {
-      center: { lat: 42, lng: -4 },
-      zoom: 5
+      center: this.center,
+      zoom: this.zoom
     };
     const mapElement = document.getElementById('map');
 
     if (mapElement) {
       const map = new google.maps.Map(mapElement, mapOptions);
-      console.log( mapOptions); 
 
       if (this.spotify.concerts && this.spotify.concerts.length > 0) {
         for (const show of this.spotify.concerts) {
-          console.log( show.venue.name, show.venue.latitude, show.venue.longitude); 
+          console.log(show.venue.name, show.venue.latitude, show.venue.longitude);
           const marker = new google.maps.Marker({
             position: { lat: show.venue.latitude, lng: show.venue.longitude },
             map: map,
             title: show.venue.name
           });
         }
-   
-
+      }
     }
   }
-  }
-
 }
